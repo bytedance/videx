f7fbcc99249b83548979bdb44acbf4db3968f514
haibo <haibo942us@gmail.com>
2025-07-08 17:30:18 -0700

    change name in build_mariadb.sh

--- build/build_mariadb.sh
+++ build/build_mariadb.sh
@@ -4,25 +4,25 @@ set -x
 
 /etc/init.d/ssh restart
 
-git config --global --add safe.directory /root/mariadb-server
+git config --global --add safe.directory /root/mariadb_server
 
-mkdir -p /root/mariadb-server/mysql_build_output/{ccache,build,data,logs,etc}
+mkdir -p /root/mariadb_server/mysql_build_output/{ccache,build,data,logs,etc}
 
-export CCACHE_DIR=/root/mariadb-server/mysql_build_output/ccache
+export CCACHE_DIR=/root/mariadb_server/mysql_build_output/ccache
 
-if [ -d /root/mariadb-server/storage/videx ]; then
+if [ -d /root/mariadb_server/storage/videx ]; then
     echo "Videx directory already exists. Skipping copy."
 else
-    echo "Copying videx to mariadb-server..."
-    cp -r /root/videx_server/src/mysql/videx /root/mariadb-server/storage/videx
+    echo "Copying videx to mariadb_server..."
+    cp -r /root/videx_server/src/mysql/videx /root/mariadb_server/storage/videx
 fi
 
-if [ -f /root/mariadb-server/mysql_build_output/etc/my.cnf ]; then
+if [ -f /root/mariadb_server/mysql_build_output/etc/my.cnf ]; then
     echo "my.cnf already exists. Skipping copy."
 else
     if [ -f /root/videx_server/build/my.cnf ]; then
-        echo "Copying my.cnf to mariadb-server..."
-        cp /root/videx_server/build/my.cnf /root/mariadb-server/mysql_build_output/etc/my.cnf
+        echo "Copying my.cnf to mariadb_server..."
+        cp /root/videx_server/build/my.cnf /root/mariadb_server/mysql_build_output/etc/my.cnf
     else
         echo "Source my.cnf not found!"
         exit 1
